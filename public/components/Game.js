52a789ba3b4a6992e8f74f06784fdc839133aa26
Glitch (invincible-tip) <none>
2022-10-26 05:56:51 +0000

    🌱🦀 Checkpoint ./public/components/Game.js:50063217/1312 ./views/index.html:50063217/16

--- public/components/Game.js
+++ public/components/Game.js
@@ -9,6 +9,10 @@ class Game {
     this._el = element
     this._status = GameStatus.Idle
     this._speed = 0
+    this._scoreBoardEl = document.querySelector('#score')
+    this._startButtonEl = document.querySelector('#start-button')
+    this._speedLabel = document.querySelector('#speed-label')
+    this._speedInput = document.querySelector('#speed-input')
   }
   render () {
     this.hydrateHeader()
@@ -16,9 +20,23 @@ class Game {
     this.start()
   }
   hydrateHeader () {
-    document.querySelector('#score').textContent = `Score: 0`
-    document.querySelector('#start-button').textContent = 'Start'
-    document.querySelector('#speed-label').textContent = `Current Speed: Slow`
+    this._scoreBoardEl.textContent = `Score: 0`
+    this._startButtonEl.textContent = 'Start'
+    this._speedLabel.textContent = `Current Speed: 0 - Difficulty: Easy`
+    this._speedInput.addEventListener('input', (e) => {
+      const speed = e.target.value
+      let speedLabel = ''
+      if (speed >= 0 && speed < 20) {
+        speedLabel = 'Easy'
+      } else if (speed >= 20 && speed < 50) {
+        speedLabel = "Medium"
+      } else if (speed >= 50 && speed < 99) {
+        speedLabel = 'Hard'
+      } else {
+        speedLabel = 'Impossible!'
+      }
+      this._speedLabel.textContent = `Current Speed: ${speed} - Difficulty: ${speedLabel}`
+    });
   }
   renderDots() {
     console.log('renderDots')
