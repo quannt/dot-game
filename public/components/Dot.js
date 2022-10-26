5a7a3a86cfa8fc5b1e8b4c2da2cb612fdc190261
Glitch (invincible-tip) <none>
2022-10-26 10:56:00 +0000

    🦀🍁 Checkpoint ./public/components/Dot.js:50063217/848 ./public/components/Game.js:50063217/74 ./public/utils/animationInterval.js:50063217/785

--- public/components/Dot.js
+++ public/components/Dot.js
@@ -1,4 +1,5 @@
 import { getRandomInt } from '../utils/number.js'
+import { animationInterval } from '../utils/animationInterval.js'
 
 class Dot {
   constructor(parentElement, xCoordinate = 0, yCoordinate = 0, type = 'div') {
@@ -11,12 +12,20 @@ class Dot {
   render () {
     const size = getRandomInt(10, 100)
     this._el = document.createElement(this._type)
-    this._el.className = 'button dot'
+    this._el.className = 'dot'
     this._el.style.height = `${size}px`
     this._el.style.width = `${size}px`
     this._el.style.transform = `translate(${this._xCoordinate}px, ${this._yCoordinate}px)`
-    
     this._parentEl.appendChild(this._el)
+    
+    this.animate()
+  }
+  animate () {
+    const controller = new AbortController();
+    animationInterval(60, controller.signal, time => {
+      this._yCoordinate = this._yCoordinate + 10
+      this._el.style.transform = `translate(${this._xCoordinate}px, ${this._yCoordinate}px)`
+    });
   }
 }
 
