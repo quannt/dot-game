4b9499c3c8885e3e6841f3100f9970c698eda840
Glitch (invincible-tip) <none>
2022-10-27 14:05:39 +0000

    👶🐥 Checkpoint ./public/store/index.js:50063217/362

--- public/store/index.js
+++ public/store/index.js
@@ -1,3 +1,20 @@
-export function store() {
-  
+let score = 0
+let speed = 0
+
+export const store = {
+  getScore () {
+    return score
+  },
+  getSpeed() {
+    return speed
+  },
+  setScore(newScore) {
+    score = newScore
+  },
+  setSpeed(newSpeed) {
+    speed = newSpeed
+  },
+  increaseScore(unit) {
+    score += unit
+  }
 }
