7084d0617b91f416126b4038b6134ea30fb8b0f7
Glitch (invincible-tip) <none>
2022-10-30 14:47:24 +0000

    🏇🍪 Checkpoint ./public/style.scss:50063217/505 ./public/components/Guide.js:50063217/444

--- public/components/Guide.js
+++ public/components/Guide.js
@@ -16,17 +16,22 @@ class Guide {
       const step = this._steps[i];
       const el = document.createElement('div');
       const hostEl = step.hostEl;
-        
+      
+      // Create the body of the guid
       const body = document.createElement('p');
       body.textContent = step.content
       el.appendChild(body);
       
+      // Create the footer menu
       const menu = document.createElement('div');
       menu.className = 'footer-menu';
       menu.innerHTML = `
-        <span>${i + 1} of ${this._steps?.length}</span>
-        <div>
-          <button class="button-link">Next »</button>
+        <div class="top-menu">
+         <span>${i + 1} of ${this._steps?.length}</span>
+         <button id="guide-next-button" class="button-link">Next »</button>
+        </div>
+        <div class="bottom-menu">
+          <button id="guide-skip-button" class="button-link">Let me play</button>
         </div>
       `;
       el.appendChild(menu);
@@ -36,7 +41,7 @@ class Guide {
       el.style.position = 'absolute';
       el.className = `guide-tooltip ${step.position}`;
       
-      // Create the footer menu
+      
       
       hostEl.style.zIndex = '2';
       hostEl.append(el)
